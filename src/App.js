f52eb374fe2d79263e7ed4893ebdb4c6dc9f578e
DominiqueDykes <nique.dykes@gmail.com>
2023-06-01 12:22:39 -0400

    added logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import logo from ".images/logo.png";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <footer className="App-footer mt-4">
+          This app was coded by Dominique Dykes and is{" "}
+          <a
+            href="https://github.com/DominiqueDykes/dictionary-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
