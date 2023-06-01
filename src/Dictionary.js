c6d22fc639ff4a6c583137eaf2cae11f4debafd6
DominiqueDykes <nique.dykes@gmail.com>
2023-06-01 13:00:36 -0400

    added apiurl

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
-import Photos from "./Photos";
+import Photos from "./photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
@@ -10,7 +10,7 @@ export default function Dictionary(props) {
   let [loaded, setLoaded] = useState(false);
   let [photos, setPhotos] = useState(null);
 
-  function handleDictionaryResponse(response) {
+  function handleDictionResponse(response) {
     setResults(response.data[0]);
   }
 
@@ -19,18 +19,15 @@ export default function Dictionary(props) {
   }
 
   function search() {
+    // documentation: https://dictionaryapi.dev/e
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleDictionaryResponse);
+    axios.get(apiUrl).then(handleDictionResponse);
 
     let pexelsApiKey =
-      "XQC0Rjri73UBaLwwXwgaaKtsnuqLByz1jqwImgZS1bpmJockHvNKpjRZ";
+      "563492ad6f91700001000001fdd29f0808df42bd90c33f42e128fa89";
     let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
-    let headers = { Authorization: `${pexelsApiKey}` };
-    axios
-      .get(pexelsApiUrl, {
-        headers: headers,
-      })
-      .then(handlePexelsResponse);
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -59,7 +56,9 @@ export default function Dictionary(props) {
               defaultValue={props.defaultKeyword}
             />
           </form>
-          <div className="hint">suggested words: sunset, wine, yoga...</div>
+          <div className="hint">
+            suggested words: sunset, wine, yoga, plant...
+          </div>
         </section>
         <Results results={results} />
         <Photos photos={photos} />
@@ -67,6 +66,6 @@ export default function Dictionary(props) {
     );
   } else {
     load();
-    return "Loading...";
+    return "Loading";
   }
 }
