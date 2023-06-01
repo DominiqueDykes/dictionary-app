26af1f230687a3dfdeaee650e20d00b921339360
DominiqueDykes <nique.dykes@gmail.com>
2023-06-01 13:48:14 -0400

    updated synonyms js

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -4,11 +4,18 @@ import "./Synonyms.css";
 export default function Synonyms(props) {
   if (props.synonyms) {
     return (
-      <ul className="Synonyms">
-        {props.synonyms.map(function (synonym, index) {
-          return <li key={index}>{synonym}</li>;
-        })}
-      </ul>
+      <div className="Synonyms mt-2">
+        <h2>Synonyms</h2>
+        <ul>
+          {props.synonyms.map(function (synonym, index) {
+            return (
+              <li key={index}>
+                {synonym} {""}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     );
   } else {
     return null;
